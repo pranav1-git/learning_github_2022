dae6ead4c8ae1105adac94abf320a2ebae54934b
Pranav@git <pranavyaduv404@gmail.com>
2022-08-16 11:56:03 +0530

    canged on monday 2nd time

--- emp.js
+++ emp.js
@@ -1,3 +1,4 @@
 console.log("I am happy with my employee")
 console.log("Never waste your time and never lose your consistency")
 console.log("Some addition on monday")
+console.log("some addition on monday 2nd time")
